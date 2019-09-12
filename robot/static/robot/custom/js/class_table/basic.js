76e65aff88ad38cdddc7bc08af1a9e7f2e39901c
QingQiz <sofeeys@outlook.com>
2019-09-12 21:06:40 +0800

    animation

--- robot/static/robot/custom/js/class_table/basic.js
+++ robot/static/robot/custom/js/class_table/basic.js
@@ -1,27 +1,46 @@
 let CalendarForm = function () {
-    $.get('/api/ct?cp=' + $('#btnSelCampus').attr('campus'), function(data, status) {
-        $('#m_calendar').fullCalendar('destroy').fullCalendar({
-            header: {
-                left: 'prev,next today',
-                    center: 'title',
-                    right: 'month,agendaWeek,agendaDay,listWeek'
-            },
-            editable: false,
-                eventLimit: true,
-            navLinks: true,
-            events: data,
-            eventRender: function (event, element) {
-                if (element.hasClass('fc-day-grid-event')) {
-                    element.data('content', event.description);
-                    element.data('placement', 'top');
-                    mApp.initPopover(element);
-                } else if (element.hasClass('fc-time-grid-event')) {
-                    element.find('.fc-title').append('<div class="fc-description">' + event.description + '</div>');
-                } else if (element.find('.fc-list-item-title').lenght !== 0) {
-                    element.find('.fc-list-item-title').append('<div class="fc-description">' + event.description + '</div>');
-                }
-            }
-        }).fullCalendar('render');
+    let cal = $('#m_calendar');
+    let aniTime = 600;
+    $('#btnSelCampus').css('pointer-events', 'none');
+    cal.css('pointer-events', 'none').animate({
+        opacity: 0,
+    }, {
+        duration: aniTime,
+        complete: function () {
+            $.get('/api/ct?cp=' + $('#btnSelCampus').attr('campus'), function (data, status) {
+                cal.fullCalendar('destroy').fullCalendar({
+                    header: {
+                        left: 'prev,next today',
+                        center: 'title',
+                        right: 'month,agendaWeek,agendaDay,listWeek'
+                    },
+                    editable: false,
+                    eventLimit: true,
+                    navLinks: true,
+                    events: data,
+                    eventRender: function (event, element) {
+                        if (element.hasClass('fc-day-grid-event')) {
+                            element.data('content', event.description);
+                            element.data('placement', 'top');
+                            mApp.initPopover(element);
+                        } else if (element.hasClass('fc-time-grid-event')) {
+                            element.find('.fc-title').append('<div class="fc-description">' + event.description + '</div>');
+                        } else if (element.find('.fc-list-item-title').lenght !== 0) {
+                            element.find('.fc-list-item-title').append('<div class="fc-description">' + event.description + '</div>');
+                        }
+                    }
+                }).fullCalendar('render');
+                cal.animate({
+                    opacity: 1
+                }, {
+                    duration: aniTime,
+                    complete: function () {
+                        cal.css('pointer-events', 'unset');
+                        $('#btnSelCampus').css('pointer-events', 'unset');
+                    }
+                });
+            });
+        }
     });
 };
 
